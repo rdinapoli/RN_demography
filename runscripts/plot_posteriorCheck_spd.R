df7a207bd7cd1b151c7fce285e99ab55183bbf8f
Enrico R. Crema <enrico.crema@gmail.com>
2020-11-08 20:28:17 +0000

    Posterior Predictive Check SPD plot -  model 4

---
+++ runscripts/plot_posteriorCheck_spd.R
@@ -0,0 +1,33 @@
+# Load Posterior Predictive Check & Observed SPDs ####
+load(here('R_imagefiles','post_pred_spd.RData'))
+load(here('R_imagefiles/','variables.RData'))
+
+# Source R Functions ####
+source(here('src','plotFunctions.R'))
+source(here('src','randomThin.R'))
+
+# Observed (thinned) SPD ####
+index=randomThin(bins)
+obs.norm =spd(cal.combined.norm[index],datenormalised=FALSE, spdnormalised = TRUE,timeRange=c(800,150),verbose=FALSE)
+obs.nnorm =spd(cal.combined.nnorm[index],datenormalised=FALSE, spdnormalised = TRUE,timeRange=c(800,150),verbose=FALSE)
+
+
+# Plot Posterior Checks
+
+
+# Model 4
+pdf(file = here('figures','temporary','posterior_predictive_check_model4_spd_euc.pdf'),width = 7,height = 8)
+par(mfrow=c(2,2),mar=c(5,4,2,1))
+plotPPCheckSPD(obs.norm,postpcheck.model4.spd[[1]],main='Calsample - normalised')
+plotPPCheckSPD(obs.nnorm,postpcheck.model4.spd[[2]],main='Calsample - non-normalised')
+plotPPCheckSPD(obs.norm,postpcheck.model4.spd[[3]],main='Uncalsample - normalised')
+plotPPCheckSPD(obs.nnorm,postpcheck.model4.spd[[4]],main='Uncalsample - non-normalised')
+dev.off()
+
+pdf(file = here('figures','temporary','posterior_predictive_check_model4_spd_nrmse.pdf'),width = 7,height = 8)
+par(mfrow=c(2,2),mar=c(5,4,2,1))
+plotPPCheckSPD(obs.norm,postpcheck.model4.spd[[5]],main='Calsample - normalised')
+plotPPCheckSPD(obs.nnorm,postpcheck.model4.spd[[6]],main='Calsample - non-normalised')
+plotPPCheckSPD(obs.norm,postpcheck.model4.spd[[7]],main='Uncalsample - normalised')
+plotPPCheckSPD(obs.nnorm,postpcheck.model4.spd[[8]],main='Uncalsample - non-normalised')
+dev.off()
