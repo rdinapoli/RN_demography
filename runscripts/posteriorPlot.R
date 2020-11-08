b888b422c2f6d560cb6f17eef74f3323ddc630ca
Enrico R. Crema <enrico.crema@gmail.com>
2020-11-08 12:18:35 +0000

    Added script for joint posterior distribution

--- runscripts/posteriorPlot.R
+++ runscripts/posteriorPlot.R
@@ -1,7 +1,7 @@
 # Load Libraries
 library(coda)
 library(here)
-library(ReIns)
+library(MASS)
 library(latex2exp)
 # Load Posteriors ####
 load(here('R_imagefiles','ABC_posteriors.RData'))
@@ -27,3 +27,30 @@ plotMarginalPosterior(post$b2,xlab=TeX('$\\beta_{soi}$'),main=TeX('$\\beta_{soi}
 lines(density(post$b2)$x,dnorm(density(post$b2)$x,mean=0,sd=0.2),lty=3,col='darkgrey',lwd=2)
 dev.off()
 }
+
+
+for (i in 1:8)
+{
+  name = names(model4.posterior)[i]
+  fn = paste0('m4_jointpost_',name,'.jpeg')
+  post = model4.posterior[[i]]
+  jpeg(file=here('figures','temporary',fn),width = 8, height = 8,res=300,units = 'in',pointsize = 12)
+  par(mfrow=c(4,4),mar=c(4,5,1,0))
+  plotMarginalPosterior(post$nt0,simple=TRUE,xlab=TeX('$N_0$'),main='')
+  replicate(3,plot(0,0,axes=F,xlab='',ylab='',type='n'))
+  image(kde2d(post$nt0,post$r,n=500),xlab=TeX('$N_0$'),ylab=TeX('$r$'),col=hcl.colors(12, "Blues 3", rev = TRUE))
+  plotMarginalPosterior(post$r,simple=TRUE,xlab=TeX('$r$'),main='')
+  replicate(2,plot(0,0,axes=F,xlab='',ylab='',type='n'))
+  image(kde2d(post$nt0,post$b1,n=500),xlab=TeX('$N_0$'),ylab=TeX('$\\beta_{palm}$'),col=hcl.colors(12, "Blues 3", rev = TRUE))
+  image(kde2d(post$r,post$b1,n=500),xlab=TeX('$r$'),ylab=TeX('$\\beta_{palm}$'),col=hcl.colors(12, "Blues 3", rev = TRUE))
+  plotMarginalPosterior(post$b1,simple=TRUE,xlab=TeX('$\\beta_{palm}$'),main='')
+  replicate(1,plot(0,0,axes=F,xlab='',ylab='',type='n'))
+  image(kde2d(post$nt0,post$b2,n=500),xlab=TeX('$N_0$'),ylab=TeX('$\\beta_{soi}$'),col=hcl.colors(12, "Blues 3", rev = TRUE))
+  image(kde2d(post$r,post$b2,n=500),xlab=TeX('$r$'),ylab=TeX('$\\beta_{soi}$'),col=hcl.colors(12, "Blues 3", rev = TRUE))
+  image(kde2d(post$b1,post$b2,n=500),xlab=TeX('$\\beta_{palm}$'),ylab=TeX('$\\beta_{soi}$'),col=hcl.colors(12, "Blues 3", rev = TRUE))
+  plotMarginalPosterior(post$b2,simple=TRUE,xlab=TeX('$\\beta_{soi}$'),main='')
+  dev.off()
+}
+
+
+
