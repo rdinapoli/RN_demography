441a666da033c9bdb667729c44723bf6ed109439
Enrico R. Crema <enrico.crema@gmail.com>
2020-11-11 20:31:03 +0000

    model 1 posteriors

--- runscripts/posteriorPlot.R
+++ runscripts/posteriorPlot.R
@@ -2,13 +2,44 @@
 library(coda)
 library(here)
 library(MASS)
+library(ReIns)
 library(latex2exp)
 # Load Posteriors ####
 load(here('R_imagefiles','ABC_posteriors.RData'))
+source(here('src','plotFunctions.R'))
+
+# Model 1 ####
+for (i in 1:8)
+{
+  name = names(model1.posterior)[i]
+  fn = paste0('m1_post_',name,'.pdf')
+  post = model1.posterior[[i]]
+  pdf(file=here('figures','temporary',fn),width = 7, height = 3.5)
+  par(mfrow=c(1,2))
+  plotMarginalPosterior(post$nt0,xlab=TeX('$N_0$'),main=TeX('$N_0$'))
+  lines(density(post$nt0)$x,dtexp(density(post$nt0)$x,rate=10,endpoint=0.5),lty=3,col='darkgrey',lwd=2)
+  plotMarginalPosterior(post$r,xlab=TeX('$r$'),main=TeX('$r$'))
+  lines(density(post$r)$x,dtexp(density(post$r)$x,rate=20,endpoint=0.5),lty=3,col='darkgrey',lwd=2)
+  dev.off()
+}
+
+for (i in 1:8)
+{
+  name = names(model1.posterior)[i]
+  fn = paste0('m1_jointpost_',name,'.jpeg')
+  post = model1.posterior[[i]]
+  jpeg(file=here('figures','temporary',fn),width = 4, height = 4,res=300,units = 'in',pointsize = 12)
+  par(mfrow=c(2,2),mar=c(4,5,1,0))
+  plotMarginalPosterior(post$nt0,simple=TRUE,xlab=TeX('$N_0$'),main='')
+  replicate(1,plot(0,0,axes=F,xlab='',ylab='',type='n'))
+  image(kde2d(post$nt0,post$r,n=500),xlab=TeX('$N_0$'),ylab=TeX('$r$'),col=hcl.colors(12, "Blues 3", rev = TRUE))
+  plotMarginalPosterior(post$r,simple=TRUE,xlab=TeX('$r$'),main='')
+  dev.off()
+}
 
 
 
-# Model 4
+# Model 4 ####
 
 for (i in 1:8)
 {
