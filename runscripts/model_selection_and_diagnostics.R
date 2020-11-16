6875d471e5edcb269d2eb17a2ccd84eac20ff6cc
Enrico R. Crema <enrico.crema@gmail.com>
2020-11-16 17:57:24 +0000

    Basic ABC diagnostic

---
+++ runscripts/model_selection_and_diagnostics.R
@@ -0,0 +1,25 @@
+library(here)
+load(here('R_imagefiles','ABC_posteriors.RData'))
+source(here('src','modelSelection.R'))
+source(here('src','tolDiagPlot.R'))
+
+# Compute Bayes Factor Matrix
+modelSelection(allmodels.posterior[[1]])
+modelSelection(allmodels.posterior[[2]])
+modelSelection(allmodels.posterior[[3]])
+modelSelection(allmodels.posterior[[4]])
+modelSelection(allmodels.posterior[[5]])
+modelSelection(allmodels.posterior[[6]])
+modelSelection(allmodels.posterior[[7]])
+modelSelection(allmodels.posterior[[8]])
+
+# Sample Diagnostic Plots ####
+
+# Model Selection
+tolDiagPlot(x=allmodels.posterior[[1]],param = 'model',tolcol='euc.cal.norm')
+
+# Posterior
+tolDiagPlot(model2.posterior[[1]],param='b1',tolcol='euc.cal.norm')
+tolDiagPlot(model4.posterior[[1]],param='b1',tolcol='euc.cal.norm')
+tolDiagPlot(model4.posterior[[1]],param='b2',tolcol='euc.cal.norm')
+tolDiagPlot(model1.posterior[[1]],param='r',tolcol='euc.cal.norm')
