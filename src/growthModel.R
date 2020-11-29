281d59c9e7c5d9472c3ad6aecd46d8bae347711a
Enrico R. Crema <enrico.crema@gmail.com>
2020-11-29 10:49:09 +0000

    Updated core growth model with new equation
    The model is now N(t) = N(t-1) * exp( r * (1- N(t-1)/exp(K))) where K is the result of a linear function a + b1 * palm + b2* soi

--- src/growthModel.R
+++ src/growthModel.R
@@ -6,7 +6,7 @@ GrowthModel = function(nt0,r,x1,x2,a,b1,b2,timeRange,raw=FALSE)
   pop[1] = nt0
   for (i in 2:length(tt))
   {
-    pop[i] = pop[i-1] * exp(r*(1-pop[i-1]/(a+b1*x1[i-1]+b2*x2[i-1])))
+    pop[i] = pop[i-1] * exp(r*(1-pop[i-1]/exp(a+b1*x1[i-1]+b2*x2[i-1])))
   }
   pop = pop[-1]
   if (raw)
