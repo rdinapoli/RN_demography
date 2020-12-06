8dfba4cdaf0fa51c7bc0b0e5efc4dff2dad60dca
Enrico R. Crema <enrico.crema@gmail.com>
2020-12-06 11:54:25 +0000

    Processed ABC results with tol=0.01

--- runscripts/process_ABC_results.R
+++ runscripts/process_ABC_results.R
@@ -3,13 +3,9 @@ library(here)
 
 # Load Raw Results ####
 load(here('R_imagefiles','abc_model1.RData'))
-model1 = result
 load(here('R_imagefiles','abc_model2.RData'))
-model2 = result
-# load(here('R_imagefiles','abc_model3.RData'))
-# model3 = result
+load(here('R_imagefiles','abc_model3.RData'))
 load(here('R_imagefiles','abc_model4.RData'))
-model4 = result
 
 # ABC settings ####
 tol=0.01
@@ -29,7 +25,7 @@ model1.posterior[[6]] = model1[order(model1$nrmse.cal.nnorm)[1:(nsim*tol)],]
 model1.posterior[[7]] = model1[order(model1$nrmse.uncal.norm)[1:(nsim*tol)],]
 model1.posterior[[8]] = model1[order(model1$nrmse.uncal.nnorm)[1:(nsim*tol)],]
 
-# model 4
+# # model 2
 model2.posterior = vector('list',length=8)
 names(model2.posterior) = c('euc.cal.norm','euc.cal.nnorm','euc.uncal.norm','euc.uncal.nnorm','nrmse.cal.norm','nrmse.cal.nnorm','nrmse.uncal.norm','nrmse.uncal.nnorm')
 model2.posterior[[1]] = model2[order(model2$euc.cal.norm)[1:(nsim*tol)],]
@@ -41,17 +37,17 @@ model2.posterior[[6]] = model2[order(model2$nrmse.cal.nnorm)[1:(nsim*tol)],]
 model2.posterior[[7]] = model2[order(model2$nrmse.uncal.norm)[1:(nsim*tol)],]
 model2.posterior[[8]] = model2[order(model2$nrmse.uncal.nnorm)[1:(nsim*tol)],]
 
-# # model 3
-# model3.posterior = vector('list',length=8)
-# names(model3.posterior) = c('euc.cal.norm','euc.cal.nnorm','euc.uncal.norm','euc.uncal.nnorm','nrmse.cal.norm','nrmse.cal.nnorm','nrmse.uncal.norm','nrmse.uncal.nnorm')
-# model3.posterior[[1]] = model3[order(model3$euc.cal.norm)[1:(nsim*tol)],]
-# model3.posterior[[2]] = model3[order(model3$euc.cal.nnorm)[1:(nsim*tol)],]
-# model3.posterior[[3]] = model3[order(model3$euc.uncal.norm)[1:(nsim*tol)],]
-# model3.posterior[[4]] = model3[order(model3$euc.uncal.nnorm)[1:(nsim*tol)],]
-# model3.posterior[[5]] = model3[order(model3$nrmse.cal.norm)[1:(nsim*tol)],]
-# model3.posterior[[6]] = model3[order(model3$nrmse.cal.nnorm)[1:(nsim*tol)],]
-# model3.posterior[[7]] = model3[order(model3$nrmse.uncal.norm)[1:(nsim*tol)],]
-# model3.posterior[[8]] = model3[order(model3$nrmse.uncal.nnorm)[1:(nsim*tol)],]
+# model 3
+model3.posterior = vector('list',length=8)
+names(model3.posterior) = c('euc.cal.norm','euc.cal.nnorm','euc.uncal.norm','euc.uncal.nnorm','nrmse.cal.norm','nrmse.cal.nnorm','nrmse.uncal.norm','nrmse.uncal.nnorm')
+model3.posterior[[1]] = model3[order(model3$euc.cal.norm)[1:(nsim*tol)],]
+model3.posterior[[2]] = model3[order(model3$euc.cal.nnorm)[1:(nsim*tol)],]
+model3.posterior[[3]] = model3[order(model3$euc.uncal.norm)[1:(nsim*tol)],]
+model3.posterior[[4]] = model3[order(model3$euc.uncal.nnorm)[1:(nsim*tol)],]
+model3.posterior[[5]] = model3[order(model3$nrmse.cal.norm)[1:(nsim*tol)],]
+model3.posterior[[6]] = model3[order(model3$nrmse.cal.nnorm)[1:(nsim*tol)],]
+model3.posterior[[7]] = model3[order(model3$nrmse.uncal.norm)[1:(nsim*tol)],]
+model3.posterior[[8]] = model3[order(model3$nrmse.uncal.nnorm)[1:(nsim*tol)],]
 
 # model 4
 model4.posterior = vector('list',length=8)
@@ -71,14 +67,12 @@ model4.posterior[[8]] = model4[order(model4$nrmse.uncal.nnorm)[1:(nsim*tol)],]
 # Model Selection ####
 model1$model='m1'
 model2$model='m2'
-# model3$model='m3'
-model4=model4[,-which(colnames(model4)=='check')]
+model3$model='m3'
 model4$model='m4'
-
-# Combine models
-allmodels = rbind.data.frame(model1,model2,model4)
-# allmodels = rbind.data.frame(model1,model2,model3,model4)
-
+ 
+## Combine models
+allmodels = rbind.data.frame(model1,model2,model3,model4)
+ 
 # Extract Posterior with tol=0.001
 tol=0.01
 allmodels.posterior = vector('list',length=8)
@@ -93,6 +87,5 @@ allmodels.posterior[[6]] = allmodels[order(allmodels$nrmse.cal.nnorm)[1:(nsim*to
 allmodels.posterior[[7]] = allmodels[order(allmodels$nrmse.uncal.norm)[1:(nsim*tol)],]
 allmodels.posterior[[8]] = allmodels[order(allmodels$nrmse.uncal.nnorm)[1:(nsim*tol)],]
 
-save(model1.posterior,model2.posterior,model4.posterior,allmodels.posterior,file=here('R_imagefiles','ABC_posteriors.RData'))
-# save(model1.posterior,model2.posterior,model3.posterior,model4.posterior,allmodel.posterior,file=here('R_imagefiles','ABC_posteriors.RData'))
+save(model1.posterior,model2.posterior,model3.posterior,model4.posterior,allmodels.posterior,file=here('R_imagefiles','ABC_posteriors.RData'))
 
