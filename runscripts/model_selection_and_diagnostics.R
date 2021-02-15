7b833c47f2bc41ba1f75bc4c76140762dd5de95e
rdinapol <rdinapol@uoregon.edu>
2021-02-15 14:22:34 -0800

    added comments to model selection

--- runscripts/model_selection_and_diagnostics.R
+++ runscripts/model_selection_and_diagnostics.R
@@ -4,14 +4,14 @@ source(here('src','modelSelection.R'))
 source(here('src','tolDiagPlot.R'))
 
 # Compute Bayes Factor Matrix
-modelSelection(allmodels.posterior[[1]])
-modelSelection(allmodels.posterior[[2]])
-modelSelection(allmodels.posterior[[3]])
+modelSelection(allmodels.posterior[[1]]) #euc.cal.norm
+modelSelection(allmodels.posterior[[2]]) #euc.cal.nnorm
+modelSelection(allmodels.posterior[[3]]) #euc.uncal.norm
 modelSelection(allmodels.posterior[[4]]) #euc.uncal.nnorm, results presented in main text
-modelSelection(allmodels.posterior[[5]])
-modelSelection(allmodels.posterior[[6]])
-modelSelection(allmodels.posterior[[7]])
-modelSelection(allmodels.posterior[[8]])
+modelSelection(allmodels.posterior[[5]]) #nrmse.cal.norm
+modelSelection(allmodels.posterior[[6]]) #nrmse.cal.nnorm
+modelSelection(allmodels.posterior[[7]]) #nrmse.uncal.norm
+modelSelection(allmodels.posterior[[8]]) #nrmse.uncal.nnorm
 
 # Sample Diagnostic Plots ####
 
